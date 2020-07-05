6e7996bb2e8e559bb7c414dee4899867f64f944b
Luis Hiram Díaz <luishiram.ds@gmail.com>
2020-07-05 00:07:12 -0500

    docs agregado

---
+++ docs/precache-manifest.6197dcb5b926281f9ee1b431e92c1228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff7def2ace4da390084bd38f00461c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46fb397d16a157655455",
+    "url": "/static/css/main.cfaf294e.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46fb397d16a157655455",
+    "url": "/static/js/main.67aad8a7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
